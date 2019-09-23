d0f5bbb73c68eac67f929b952fcc49f99b70890b
krenares <55036085+krenares@users.noreply.github.com>
2019-09-23 14:26:20 -0500

    Update 6thexercise.py

--- 6thexercise.py
+++ 6thexercise.py
@@ -1,6 +1,6 @@
 def is_leap(year):
     leap = False
-    if year % 4 == 0and 
+    if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
                 return True
@@ -12,4 +12,4 @@ def is_leap(year):
     return leap
 
 year = int(input())
-print(is_leap(year))
+print(is_leap(year)
